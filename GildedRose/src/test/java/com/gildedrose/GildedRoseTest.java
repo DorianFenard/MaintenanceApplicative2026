6a124ceaeae123df26bd3c27c7d234361efbcaab
DorianFenard <dorian.fenard@gmail.com>
2026-03-11 14:48:46 +0100

    ajout commentaire test

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,8 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    /**
+     * Test de l'article normal avant la date de vente 
+     */
     @Test
-    void articleNormalAvantDateDeVente() {
+    void normalItemBeforeSellDate() {
         Item[] items = new Item[] { new Item("foo", 1, 5) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -16,8 +19,11 @@ class GildedRoseTest {
         assertEquals(4, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article normal le jour de la vente
+     */
     @Test
-    void articleNormalAuJourDeVente() {
+    void normalItemOnSellDate() {
         Item[] items = new Item[] { new Item("foo", 0, 5) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -25,8 +31,11 @@ class GildedRoseTest {
         assertEquals(3, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article normal après la date de vente
+     */
     @Test
-    void agedBrieAugmentation() {
+    void agedBrieIncreases() {
         Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -34,8 +43,11 @@ class GildedRoseTest {
         assertEquals(1, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article normal après la date de vente
+     */
     @Test
-    void agedBrieApresDateDoubleAugmentation() {
+    void agedBrieAfterSellDateIncreasesTwice() {
         Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -43,8 +55,11 @@ class GildedRoseTest {
         assertEquals(2, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article Sulfuras qui ne change jamais
+     */
     @Test
-    void sulfurasNeChangeJamais() {
+    void sulfurasNeverChanges() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -52,8 +67,11 @@ class GildedRoseTest {
         assertEquals(80, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article Backstage Passes qui augmente de 1 avant 10 jours, de 2 entre 10 et 5 jours, de 3 entre 5 et 0 jours et qui tombe à 0 après le concert
+     */
     @Test
-    void backstageBienAvantDate() {
+    void backstageLongBeforeSellDate() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -61,8 +79,11 @@ class GildedRoseTest {
         assertEquals(21, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article Backstage Passes qui augmente de 1 avant 10 jours, de 2 entre 10 et 5 jours, de 3 entre 5 et 0 jours et qui tombe à 0 après le concert
+     */
     @Test
-    void backstageProcheMoyennementDate() {
+    void backstageMediumCloseToSellDate() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -70,8 +91,11 @@ class GildedRoseTest {
         assertEquals(22, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article Backstage Passes qui augmente de 1 avant 10 jours, de 2 entre 10 et 5 jours, de 3 entre 5 et 0 jours et qui tombe à 0 après le concert
+     */
     @Test
-    void backstageTresProcheDate() {
+    void backstageVeryCloseToSellDate() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -80,7 +104,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void backstageApresConcertQualiteAZero() {
+    void backstageAfterConcertQualityDropsToZero() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -88,21 +112,29 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].quality);
     }
 
+    /**
+     * Test de l'article normal après la date de vente
+     */
     @Test
-    void qualiteJamaisNegative() {
+    void qualityNeverNegative() {
         Item[] items = new Item[] { new Item("foo", 5, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(0, app.items[0].quality);
     }
-
+/**
+     * Test de l'article normal après la date de vente
+     */
     @Test
-    void qualiteJamaisSuperieureA50() {
+    void qualityNeverExceeds50() {
         Item[] items = new Item[] { new Item("Aged Brie", 5, 50) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(50, app.items[0].quality);
     }
+    /**
+     * Test de la méthode toString de l'article
+     */
     @Test
     void itemToString(){
         Item[] items = new Item[] { new Item("Aged Brie", 5, 50) };
